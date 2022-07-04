2cf317991218023f6f41cfa59b8322e3fc7d59cd
Sarahcardoso134 <99329372+Sarahcardoso134@users.noreply.github.com>
2022-07-04 13:43:38 -0300

    Add files via upload

---
+++ rata/sarahlivrarianovo/jnvfkjdhgiordjf/novooolivros/atividadepr/Livaria-My/livraria/src/components/template/FooterComp.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer></footer>
+</template>
+<style>
+footer {
+  background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaViVx1n1mNlI8aFPoXnlVy3KZDoBe1NLf5Q&usqp=CAU);
+  background-color: #000000;
+  height: 15%;
+  font-size: 1.3rem;
+  padding-left: 2rem;
+  color: #f3f3f3;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ rata/sarahlivrarianovo/jnvfkjdhgiordjf/novooolivros/atividadepr/Livaria-My/livraria/src/components/template/HeaderComp.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <div>
+      <span> <RouterLink to="/editoras">Editoras</RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/categorias">Categorias</RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/autores">Autores</RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/livros">Livros</RouterLink> </span>
+    </div>
+    <div>
+      <span> <RouterLink to="/login">Login</RouterLink> </span>
+    </div>
+  </header>
+</template>
+
+<style>
+header span {
+  padding: 0 20px;
+}
+
+header {
+  background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaViVx1n1mNlI8aFPoXnlVy3KZDoBe1NLf5Q&usqp=CAU);
+  background-color: #000000;
+  height: 15%;
+  font-size: 1.3rem;
+  padding-left: 2rem;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
